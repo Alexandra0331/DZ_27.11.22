7f9214391524890efedde589eedd52989755c007
Alexandra <Kaverzina0331@mail.ru>
2022-12-02 17:12:28 +0300

    2.py

---
+++ 2.py
@@ -0,0 +1,22 @@
+# 2. Напишите программу, которая найдёт произведение пар чисел списка.
+# Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+# in
+# 4
+
+# out
+# [2, 5, 8, 10]
+# [20, 40]
+
+# in
+# 5
+
+# out
+# [2, 2, 4, 8, 8]
+# [16, 16, 4]
+lst = []
+lst = list(map(int, input("Введите числа через пробел:\n").split()))
+def mult_lst(lst):
+    l = len(lst)//2 + 1 if len(lst) % 2 != 0 else len(lst)//2
+    new_lst = [lst[i]*lst[len(lst)-i-1] for i in range(l)]
+    print(new_lst)
+mult_lst(lst)
